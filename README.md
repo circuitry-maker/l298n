8f012557b2050fa372d12737aa09684cf9c12fa5
luca <lucagiuggia@gmail.com>
2019-10-15 23:02:09 +0200

    Improve documentation and add single motor implementation

--- README.md
+++ README.md
@@ -2,11 +2,32 @@
 
 > no_std driver for [L298N](https://www.st.com/resource/en/datasheet/l298.pdf) (Dual H-Bridge Motor Controller module)
 
+[![Build Status](https://travis-ci.org/lucazulian/l298n.svg?branch=master)](https://travis-ci.org/lucazulian/l298n)
+
 ## Basic usage
+
+Include this [library](https://crates.io/crates/l298n) as a dependency in your `Cargo.toml`:
+
+```rust
+[dependencies.l298n]
+version = "<version>"
+```
+Use [embedded-hal](https://github.com/rust-embedded/embedded-hal) implementation to get PINA, PINB and PWM and then create l298n single motor:
+
+```rust
+extern crate l298n;
+
+let motor = l298n::Motor::new(PINA, PINB, PWM);
+
+motoa.set_duty(12); 
+
+motor.brake();
+
+```
 
 ## Documentation
 
-API Docs available on [docs.rs](https://docs.rs/l298n)
+API Docs available on [docs.rs](https://docs.rs/l298n/0.1.0/l298n/)
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,89 @@
 //! Constructs a new
 //!
 //! 
+
+use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::PwmPin;
+
+/// dummy
+pub struct Motor<IN1, IN2, PWM>
+where
+    IN1: OutputPin,
+    IN2: OutputPin,
+    PWM: PwmPin,
+{
+    in1: IN1,
+    in2: IN2,
+    pwm: PWM,
+}
+
+
+impl<IN1, IN2, PWM> Motor<IN1, IN2, PWM>
+where
+    IN1: OutputPin,
+    IN2: OutputPin,
+    PWM: PwmPin,
+{
+    /// dummy
+    pub fn new(in1: IN1, in2: IN2, pwm:PWM) -> Motor<IN1, IN2, PWM>
+    where
+        IN1: OutputPin,
+        IN2: OutputPin,
+        PWM: PwmPin,
+        {
+
+            Motor {
+                in1: in1,
+                in2: in2,
+                pwm: pwm
+            }
+    }
+
+    /// Brakes the motor - Fast Motor Stop
+    /// with Ven = H then C = D Fast Motor Stop
+    pub fn brake(&mut self) -> &mut Self {
+        self.in1.set_high().ok();
+        self.in2.set_high().ok();
+        self
+    }
+
+    /// Stops the motor - Free Running Motor Stop
+    /// Ven = L then with C = X ; D = X
+    pub fn stop(&mut self) -> &mut Self {
+        self.in1.set_high().ok();
+        self.in2.set_high().ok();
+        self
+    }
+
+    /// Makes the motor forward direction
+    /// with Ven = H then C = H ; D = L Forward
+    pub fn forward(&mut self) -> &mut Self {
+        self.in1.set_low().ok();
+        self.in2.set_high().ok();
+        self
+    }
+
+    /// Makes the motor reverse direction
+    /// with Ven = H then C = L ; D = H Reverse
+    pub fn reverse(&mut self) -> &mut Self {
+        self.in1.set_high().ok();
+        self.in2.set_low().ok();
+        self
+    }
+
+    /// Returns the maximum
+    pub fn get_max_duty(&mut self) -> PWM::Duty {
+        self.pwm.get_max_duty()
+    }
+
+    /// Changes the motor speed
+    pub fn set_duty(&mut self, duty: PWM::Duty) -> &mut Self {
+        self.pwm.set_duty(duty);
+        self
+    }
+
+    /// Get the actual motor speed
+    pub fn get_current_duty(&mut self) -> PWM::Duty {
+        self.pwm.get_duty()
+    }
+}
